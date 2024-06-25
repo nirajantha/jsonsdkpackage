2038e0e8c8526f2584d0f0ff907fd7cbe9049616
Nirajan thapa <nnirajanthapaa@gmail.com>
2024-06-25 10:12:14 +0545

    get all posts features added

--- test/test.js
+++ test/test.js
@@ -1,6 +1,15 @@
-const jsonSdk = require("../index")
+const jsonSdk = require("jsonsdkpackage")
 const sdk = new jsonSdk();
 
-sdk.getAllUsers()
-.then(users=>console.log("users>>>",users))
-.catch(error=>console.log("errors>>",error))
+// sdk.getAllUsers()
+// .then(users=>console.log("users>>>",users))
+// .catch(error=>console.log("errors>>",error))
+
+// const res = sdk.getAllUsers().then(users=>users)
+// console.log("res>>",res)
+
+async function fetchUsers(){
+   const res =  await sdk.getAllUsers()
+   console.log("res>>",res)
+}
+fetchUsers();
